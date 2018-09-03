5326e396999fc5a0ec49bf31c846df074cc4c418
Dawid Dutkiewicz <dawid.dutkiewicz@gmail.com>
2018-09-03 18:56:44 +0200

    - initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='adform',
+      version='0.1dev',
+      description='Unofficial AdForm.com REST API client.',
+      url='https://github.com/dutkiewicz/adform-api/',
+      author='Dawid Dutkiewicz',
+      author_email='dawid.dutkiewicz@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'requests==2.19.1'
+      ],
+      python_requires='>=3')
